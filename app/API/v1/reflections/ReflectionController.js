cbf6ac961ec817da9c6dd33c371c316a7337291a
alit789 <56476878+alit789@users.noreply.github.com>
2023-10-30 11:06:57 +0800

    add get all reflection

--- app/API/v1/reflections/ReflectionController.js
+++ app/API/v1/reflections/ReflectionController.js
@@ -1,37 +1,53 @@
 const { pool } = require("../../../db/index");
 
-
 async function createReflection(req, res) {
   const { success, low_point, take_away } = req.body;
   const userId = req.user.id;
 
   try {
-    const insertQuery = `
+    const postQuery = `
       INSERT INTO Reflections (success, low_point, take_away, UserId)
       VALUES ($1, $2, $3, $4)
       RETURNING *
     `;
 
     const values = [success, low_point, take_away, userId];
-    const newReflection = await pool.query(insertQuery, values);
+    const newReflection = await pool.query(postQuery, values);
 
     const response = {
       success: newReflection.rows[0].success,
       low_point: newReflection.rows[0].low_point,
       take_away: newReflection.rows[0].take_away,
       UserId: newReflection.rows[0].UserId,
-      createdAt: newReflection.rows[0].createdat, 
-      updatedAt: newReflection.rows[0].updatedat, 
+      createdAt: newReflection.rows[0].createdat,
+      updatedAt: newReflection.rows[0].updatedat,
     };
 
     res.status(201).json(response);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     console.error("Error creating reflection:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+async function getReflection(req, res) {
+  const userId = req.user.id;
+
+  try {
+    const getQuery = `SELECT id, success, low_point, take_away, UserId, createdAt, updatedAt
+    FROM Reflections
+    WHERE UserId = $1`;
+    const value = [userId];
+    const reflections = await pool.query(getQuery, value);
+    res.status(200).json(reflections.rows);
+  } catch (error) {
+    console.error("Error fetching reflections:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
 
 module.exports = {
   createReflection,
+  getReflection
 };
--- app/API/v1/reflections/ReflectionRoutes.js
+++ app/API/v1/reflections/ReflectionRoutes.js
@@ -3,5 +3,6 @@ const ReflectionController = require('./ReflectionController')
 const authentication = require("../../../../middleware/authentication");
 
 router.post('/',authentication, ReflectionController.createReflection)
+router.get('/',authentication, ReflectionController.getReflection)
 
 module.exports = router
